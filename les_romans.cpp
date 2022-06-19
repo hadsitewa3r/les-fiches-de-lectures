b7c2ab69414839396faeb3429624d617c4b0d3fd
Hamza <98172837+hadsitewa3r@users.noreply.github.com>
2022-06-19 21:42:46 +0100

    Update les_romans.cpp

--- les_romans.cpp
+++ les_romans.cpp
@@ -10,30 +10,28 @@ public:
 	int date_décriture;
 	int date_de_parution;
 	const char* genre;
+	
+	Roman(const char* Titre, const char* Auteur, int DateNessance, int DateMort , int Siécle, int DateEcrit, int DateParution, const char* Genre):
+		titre(Titre), auteur(Auteur), date_néssance_dauteur(DateNessance), date_mort_dauteur(DateMort),
+		siécle(Siécle), date_décriture(DateEcrit), date_de_parution(DateParution), genre(Genre){}
 
-    Roman(const char* Titre, const char* Auteur, int DateNessance, int DateMort , int Siécle, int DateEcrit, int DateParution, const char* Genre){
-        titre = Titre; auteur = Auteur; date_néssance_dauteur = DateNessance; date_mort_dauteur = DateMort;
-        siécle = Siécle; date_décriture = DateEcrit; date_de_parution = DateParution; genre = Genre;
-    }
-
-    ~Roman(){
-        //7tta idoz ljihawi o nsa dakchi
-        
-    }
+	~Roman(){
+		//7tta idoz ljihawi o nsa dakchi
+	}
 };
 
-int main (){
+int main (int argc, char** argv){
 
-    Roman la_boite("la boite à merveilles", "Ahmed SEFRIOUI", 1915, 2004, 20, 1952, 1954, "Roman autobiographique");
-    Roman antigone("Antigone", "Jean ANOUILH",1910 ,1987 ,20 , 1942, 1944, "Trajédie Moderne");
-    Roman le_dernier_jour("le dernier jour d'un condamné", "Victor HUGO",1802 ,1885 , 19, 1827, 1829, "Roman à thése");
+	Roman la_boite("la boite à merveilles", "Ahmed SEFRIOUI", 1915, 2004, 20, 1952, 1954, "Roman autobiographique");
+	Roman antigone("Antigone", "Jean ANOUILH",1910 ,1987 ,20 , 1942, 1944, "Trajédie Moderne");
+	Roman le_dernier_jour("le dernier jour d'un condamné", "Victor HUGO",1802 ,1885 , 19, 1827, 1829, "Roman à thése");
 
 	printf("\n*******************************************\n||[\tFICHES DE LECTURE DES ROMANS\t]||\n*******************************************\n\n\n");
 
 	Roman* oeuvre;
 
-	for(unsigned char i = 0; i < 3; i++){
-		if(i == 0) oeuvre = &la_boite;
+	for(unsigned char i = 0; (i*5) < (5*3); i++){
+		if(!i)     oeuvre = &la_boite;
 		if(i == 1) oeuvre = &antigone;
 		if(i == 2) oeuvre = &le_dernier_jour;
 
